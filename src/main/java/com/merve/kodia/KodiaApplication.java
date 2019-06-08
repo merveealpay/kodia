bd84debfb93e288741fba800244414e4856bf84e
merve <merveealpay@gmail.com>
2019-06-08 14:49:18 +0300

    Spring MVC is created

---
+++ src/main/java/com/merve/kodia/KodiaApplication.java
@@ -0,0 +1,14 @@
+package com.merve.kodia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodiaApplication.class, args);
+		
+	}
+
+}
